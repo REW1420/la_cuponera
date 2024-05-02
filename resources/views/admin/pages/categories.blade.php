2fa170123f5e637859575d19f81b278ea57dc0f9
Jaeli20 <werramos1420@gmail.com>
2024-05-02 12:52:10 -0600

    new table create

--- resources/views/admin/pages/categories.blade.php
+++ resources/views/admin/pages/categories.blade.php
@@ -26,8 +26,7 @@ $a = '';
 
         <!-- Table -->
         <div class="table-responsive">
-            <table class="table table-hover">
-
+            <table id="categoryTable" class="table table-hover" style="width: 100%">
                 <thead>
                     <tr>
                         <th scope="col">ID</th>
@@ -36,39 +35,31 @@ $a = '';
                     </tr>
                 </thead>
                 <tbody>
-
                     @foreach ($categories as $category)
-                        <th scope="row">{{ $category->id }}</th>
-                        <td>{{ $category->name }}</td>
-
-
-
-                        <td>
-                            <div class="d-flex flex-row  mb-3">
-                                <div>
-                                    <a href="#" class="btn" data-bs-toggle="modal"
-                                        data-bs-target="#editModal{{ $category->id }}"> <i
-                                            class="material-icons text-warning">Editar</i></a>
+                        <tr>
+                            <td scope="row">{{ $category->id }}</td>
+                            <td>{{ $category->name }}</td>
+                            <td>
+                                <div class="d-flex flex-row mb-3">
+                                    <div>
+                                        <a href="#" class="btn" data-bs-toggle="modal"
+                                            data-bs-target="#editModal{{ $category->id }}">
+                                            <i class="material-icons text-warning">Editar</i>
+                                        </a>
+                                    </div>
+                                    <div>
+                                        <a href="#" class="btn" data-bs-toggle="modal"
+                                            data-bs-target="#deleteModal{{ $category->id }}">
+                                            <i class="material-icons text-danger">Eliminar</i>
+                                        </a>
+                                    </div>
                                 </div>
-
-
-                                <div>
-                                    <a href="#" class="btn" data-bs-toggle="modal"
-                                        data-bs-target="#deleteModal{{ $category->id }}"> <i
-                                            class="material-icons text-danger">Eliminar</i></a>
-                                </div>
-
-
-
-                            </div>
-                        </td>
-                        @include('modals.category.delete')
-                        @include('modals.category.edit')
+                            </td>
                         </tr>
                     @endforeach
-
                 </tbody>
             </table>
+
         </div>
 
 
@@ -87,18 +78,59 @@ $a = '';
         integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous">
     </script>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src='https://cdn.datatables.net/1.13.5/js/jquery.dataTables.min.js'></script>
+    <script src='https://cdn.datatables.net/responsive/2.1.0/js/dataTables.responsive.min.js'></script>
+    <script src='https://cdn.datatables.net/1.13.5/js/dataTables.bootstrap5.min.js'></script>
 
     <script>
         $(document).ready(function() {
-            $('.deleteBtb').click(function(e) {
-                e.preventDefault();
-                var id = $(this).val();
-                $("#item_id").val(id);
-                $('#deleteModal').modal('show')
-                console.log('aa')
-            });
+            $('#categoryTable').DataTable({
+                //disable sorting on last column
+                "columnDefs": [{
+                    "orderable": false,
+                    "targets": 2
+                }],
+                language: {
+                    "sProcessing": "Procesando...",
+                    "sLengthMenu": "Mostrar _MENU_ registros",
+                    "sZeroRecords": "No se encontraron resultados",
+                    "sEmptyTable": "Ningún dato disponible en esta tabla",
+                    "sInfo": "Mostrando registros del _START_ al _END_ de un total de _TOTAL_ registros",
+                    "sInfoEmpty": "Mostrando registros del 0 al 0 de un total de 0 registros",
+                    "sInfoFiltered": "(filtrado de un total de _MAX_ registros)",
+                    "sInfoPostFix": "",
+                    "sSearch": "Buscar:",
+                    "sUrl": "",
+                    "sInfoThousands": ",",
+                    "sLoadingRecords": "Cargando...",
+                    "oPaginate": {
+                        "sFirst": "Primero",
+                        "sLast": "Último",
+                        "sNext": "Siguiente",
+                        "sPrevious": "Anterior"
+                    },
+                    //customize pagination prev and next buttons: use arrows instead of words
+                    'paginate': {
+                        'previous': '<span class="fa fa-chevron-left">-</span>',
+                        'next': '<span class="fa fa-chevron-right">+</span>'
+                    },
+                    //customize number of elements to be displayed
+                    "lengthMenu": 'Mostrar <select class="form-control input-sm">' +
+                        '<option value="5">5</option>' + // Nueva opción para mostrar 5 elementos por página
+                        '<option value="10">10</option>' +
+                        '<option value="20">20</option>' +
+                        '<option value="30">30</option>' +
+                        '<option value="40">40</option>' +
+                        '<option value="50">50</option>' +
+                        '<option value="-1">All</option>' +
+                        '</select> '
+
+                }
+            })
         });
     </script>
+
+
 
 
 
